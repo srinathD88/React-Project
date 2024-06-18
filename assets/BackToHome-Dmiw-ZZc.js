7f89cc807b94f680a33efb0b03dbcf43306db298
rac <rac@racs-MBP.domain.name>
2024-06-18 16:01:42 +0530

    Updates

--- assets/BackToHome-Dmiw-ZZc.js
+++
@@ -1,1 +0,0 @@
-import{j as o,L as s,R as r}from"./index-_xUAHA8x.js";const e=()=>o.jsx("p",{children:o.jsx(s,{to:r.HOME,children:"Back to Home"})});export{e as B};
---
+++ assets/withPageTitle-DJjxIVNU.js
@@ -0,0 +1,1 @@
+import{r as s,j as o}from"./index-9IkskKFm.js";const n=(r,t)=>e=>(s.useEffect(()=>{document.title=t},[t]),o.jsx(r,{...e}));export{n as w};
